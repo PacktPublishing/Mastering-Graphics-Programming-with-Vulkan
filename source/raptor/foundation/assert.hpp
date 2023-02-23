469d625eda754dfdd7fa0778da352625ed9ccecc
Marco Castorina <marco@marcocastorina.com>
2023-02-23 09:33:13 +0000

    Fixed typo when detecting msvc

--- source/raptor/foundation/assert.hpp
+++ source/raptor/foundation/assert.hpp
@@ -5,7 +5,7 @@
 namespace raptor {
 
     #define RASSERT( condition )      if (!(condition)) { rprint(RAPTOR_FILELINE("FALSE\n")); RAPTOR_DEBUG_BREAK }
-#if defined(_MSV_VER)
+#if defined(_MSC_VER)
     #define RASSERTM( condition, message, ... ) if (!(condition)) { rprint(RAPTOR_FILELINE(RAPTOR_CONCAT(message, "\n")), __VA_ARGS__); RAPTOR_DEBUG_BREAK }
 #else
     #define RASSERTM( condition, message, ... ) if (!(condition)) { rprint(RAPTOR_FILELINE(RAPTOR_CONCAT(message, "\n")), ## __VA_ARGS__); RAPTOR_DEBUG_BREAK }
